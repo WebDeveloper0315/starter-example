a2788c40039184a7bec72f70b11928ea729941ff
Blackghost <Blackghost1503@gmail.com>
2024-01-10 09:04:10 -0800

    seed

--- app/page.tsx
+++ app/page.tsx
@@ -17,9 +17,9 @@ export default function Page() {
             className={styles.shape}
           />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Happy New Year!</strong> This is the blog for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              Next.js developers in 2024
             </a>
             , brought to you by Vercel.
           </p>
